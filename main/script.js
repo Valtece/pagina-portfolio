8d56f8ea1bb5b393de82beb702d67934d15be161
Valtece Ventena <105180137+Valtece@users.noreply.github.com>
2022-05-30 21:19:56 -0300

    Update

--- main/script.js
+++ main/script.js
@@ -11,3 +11,13 @@ function digitacao(texto, contador) {
 }
 
 digitacao("Crio seu site simples e moderno.", 0);
+
+$('.nav a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+  targetOffset = $(id).offset().top;
+    
+  $('html, body').animate({ 
+    scrollTop: targetOffset - 100
+  }, 500);
+});
